671eb739ac8e42b35836763ea074f3ca15faf8e4
Duarte1104 <pereiraduarte1104@gmail.com>
2025-03-20 22:42:15 +0000

    NovoLayout

--- TalentosIT/Controllers/PerfilTalentoController.cs
+++ TalentosIT/Controllers/PerfilTalentoController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using TalentosIT.Models;
 
 [Route("api/[controller]")]
@@ -13,12 +16,14 @@ public class PerfilTalentoController : ControllerBase
         _context = context;
     }
 
+    // GET: api/PerfilTalento
     [HttpGet]
     public async Task<ActionResult<IEnumerable<PerfilTalento>>> GetPerfisTalento()
     {
         return await _context.PerfilTalentos.ToListAsync();
     }
 
+    // GET: api/PerfilTalento/{id}
     [HttpGet("{id}")]
     public async Task<ActionResult<PerfilTalento>> GetPerfilTalento(int id)
     {
@@ -30,5 +35,66 @@ public class PerfilTalentoController : ControllerBase
         }
 
         return perfilTalento;
+    }
+
+    // POST: api/PerfilTalento
+    [HttpPost]
+    public async Task<ActionResult<PerfilTalento>> PostPerfilTalento(PerfilTalento perfilTalento)
+    {
+        _context.PerfilTalentos.Add(perfilTalento);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction(nameof(GetPerfilTalento), new { id = perfilTalento.Cod }, perfilTalento);
+    }
+
+    // PUT: api/PerfilTalento/{id}
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutPerfilTalento(int id, PerfilTalento perfilTalento)
+    {
+        if (id != perfilTalento.Cod)
+        {
+            return BadRequest();
+        }
+
+        _context.Entry(perfilTalento).State = EntityState.Modified;
+
+        try
+        {
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!PerfilTalentoExists(id))
+            {
+                return NotFound();
+            }
+            else
+            {
+                throw;
+            }
+        }
+
+        return NoContent();
+    }
+
+    // DELETE: api/PerfilTalento/{id}
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeletePerfilTalento(int id)
+    {
+        var perfilTalento = await _context.PerfilTalentos.FindAsync(id);
+        if (perfilTalento == null)
+        {
+            return NotFound();
+        }
+
+        _context.PerfilTalentos.Remove(perfilTalento);
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
+
+    private bool PerfilTalentoExists(int id)
+    {
+        return _context.PerfilTalentos.Any(e => e.Cod == id);
     }
 }
--- TalentosIT/Controllers/PropostaSkillController.cs
+++ TalentosIT/Controllers/PropostaSkillController.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using System.Collections.Generic;
-using System.Threading.Tasks;
+
 
 [Route("api/[controller]")]
 [ApiController]
@@ -14,12 +13,14 @@ public class PropostaSkillController : ControllerBase
         _context = context;
     }
 
+    // GET: api/PropostaSkill
     [HttpGet]
     public async Task<ActionResult<IEnumerable<PropostaSkill>>> GetPropostaSkills()
     {
         return await _context.PropostaSkills.ToListAsync();
     }
 
+    // GET: api/PropostaSkill/{idPropostaTrabalho}/{codSkill}
     [HttpGet("{idPropostaTrabalho}/{codSkill}")]
     public async Task<ActionResult<PropostaSkill>> GetPropostaSkill(int idPropostaTrabalho, int codSkill)
     {
@@ -31,5 +32,64 @@ public class PropostaSkillController : ControllerBase
         }
 
         return propostaSkill;
+    }
+    
+    // PUT: api/PropostaSkill/{idPropostaTrabalho}/{codSkill}
+    [HttpPut("{idPropostaTrabalho}/{codSkill}")]
+    public async Task<IActionResult> PutPropostaSkill(int idPropostaTrabalho, int codSkill, PropostaSkill propostaSkill)
+    {
+        if (idPropostaTrabalho != propostaSkill.IdPropostaTrabalho || codSkill != propostaSkill.CodSkill)
+        {
+            return BadRequest("Os IDs fornecidos na URL não correspondem aos IDs no corpo da requisição.");
+        }
+
+        _context.Entry(propostaSkill).State = EntityState.Modified;
+
+        try
+        {
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!_context.PropostaSkills.Any(e => e.IdPropostaTrabalho == idPropostaTrabalho && e.CodSkill == codSkill))
+            {
+                return NotFound();
+            }
+            else
+            {
+                throw;
+            }
+        }
+
+        return NoContent();
+    }
+
+
+    // POST: api/PropostaSkill
+    [HttpPost]
+    public async Task<ActionResult<PropostaSkill>> PostPropostaSkill(PropostaSkill propostaSkill)
+    {
+        _context.PropostaSkills.Add(propostaSkill);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction(nameof(GetPropostaSkill), 
+            new { idPropostaTrabalho = propostaSkill.IdPropostaTrabalho, codSkill = propostaSkill.CodSkill }, 
+            propostaSkill);
+    }
+
+    // DELETE: api/PropostaSkill/{idPropostaTrabalho}/{codSkill}
+    [HttpDelete("{idPropostaTrabalho}/{codSkill}")]
+    public async Task<IActionResult> DeletePropostaSkill(int idPropostaTrabalho, int codSkill)
+    {
+        var propostaSkill = await _context.PropostaSkills.FindAsync(idPropostaTrabalho, codSkill);
+        if (propostaSkill == null)
+        {
+            return NotFound();
+        }
+
+        _context.PropostaSkills.Remove(propostaSkill);
+        await _context.SaveChangesAsync();
+
+        return NoContent();
     }
 }
