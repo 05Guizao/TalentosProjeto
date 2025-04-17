bfb45e1f8126e5391cb96a81507da97146600a7b
FranciscoOMatos <franciscomatos@ipvc.pt>
2025-04-17 16:31:00 +0100

    commit

---
+++ TalentosIT/Models/SkillAtiva.cs
@@ -0,0 +1,12 @@
+﻿namespace TalentosIT.Models
+{
+    public class SkillAtiva : SkillBase
+    {
+        public override string Estado { get; set; } = "Ativo";
+
+        public override bool EhValida()
+        {
+            return true;
+        }
+    }
+}
