bf28f0cf9d9c208bf560e7a40dd90e8f2c766ea1
guilhermebarros <g.barros@ipvc.pt>
2025-05-31 17:12:34 +0100

    Commit

--- TalentosIT/obj/Debug/net9.0/TalentosIT.AssemblyInfo.cs
+++ TalentosIT/obj/Debug/net9.0/TalentosIT.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TalentosIT")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2eaed0d91c16e7f44ec68d8cc6244c81db61fd46")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+606990edc435cdadf7783558b275b30e2f531512")]
 [assembly: System.Reflection.AssemblyProductAttribute("TalentosIT")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TalentosIT")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
