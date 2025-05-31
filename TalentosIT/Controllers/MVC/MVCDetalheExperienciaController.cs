2eaed0d91c16e7f44ec68d8cc6244c81db61fd46
guilhermebarros <g.barros@ipvc.pt>
2025-05-31 16:27:07 +0100

    Commit

--- TalentosIT/Controllers/MVC/MVCDetalheExperienciaController.cs
+++ TalentosIT/Controllers/MVC/MVCDetalheExperienciaController.cs
@@ -42,5 +42,34 @@ namespace TalentosIT.Controllers
 
             return View(experiencia);
         }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var experiencia = await _service.ObterPorIdAsync(id);
+            if (experiencia == null) return NotFound();
+            return View(experiencia);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(DetalheExperiencia experiencia)
+        {
+            if (ModelState.IsValid)
+            {
+                await _service.AtualizarAsync(experiencia);
+                return RedirectToAction("Index");
+            }
+
+            return View(experiencia);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _service.EliminarAsync(id);
+            return RedirectToAction("Index");
+        }
     }
 }
--- TalentosIT/Services/IDetalheExperienciaService.cs
+++ TalentosIT/Services/IDetalheExperienciaService.cs
@@ -1,12 +1,15 @@
-﻿using TalentosIT.Models;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
+using TalentosIT.Models;
 
 namespace TalentosIT.Services
 {
     public interface IDetalheExperienciaService
     {
         List<DetalheExperiencia> ObterPorUtilizador(int userId);
+        Task<DetalheExperiencia> ObterPorIdAsync(int id);
         Task CriarAsync(int userId, DetalheExperiencia experiencia);
+        Task AtualizarAsync(DetalheExperiencia experiencia);
+        Task EliminarAsync(int id); // <- este é o nome correto para corresponder ao controller e à implementação
     }
 }
