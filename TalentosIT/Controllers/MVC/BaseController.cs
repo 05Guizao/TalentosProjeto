35f353fc53a95c33f2ba80a08b4dbfe903690573
paulosimoess <paulosimoes@ipvc.pt>
2025-06-09 19:26:08 +0100

    commit

---
+++ TalentosIT/Controllers/MVC/BaseController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TalentosIT.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected void SetUserInViewBag()
+        {
+            ViewBag.Nome = HttpContext.Session.GetString("UserNome");
+            ViewBag.Tipo = HttpContext.Session.GetString("UserTipo");
+        }
+    }
+}
